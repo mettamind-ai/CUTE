60815d96f2480a1c6e59aa8a4d40a731d2d30f87
Tien Dung <dungtn@gmail.com>
2025-05-23 08:06:09 +0000

    update

--- bench.sh
+++ bench.sh
@@ -1,7 +1,7 @@
 
-./pretrain.py --S --steps 1125 --bs 24 --exits 1                 # minimal
-./pretrain.py --S --steps 1125 --bs 24 --exits 1 --te 99 --ve 99 # max-inp
-./pretrain.py --S --steps 1125 --bs 24 --exits 3 --future 10     # max-out
+./pretrain.py --S --bs 16 --exits 1                 # minimal
+./pretrain.py --S --bs 16 --exits 1 --te 99 --ve 99 # max-inp
+./pretrain.py --S --bs 16 --exits 3 --future 10     # max-out
 
 # ./pretrain.py --M --steps 1125 --bs 16    #  64k
 # ./pretrain.py --M --steps  750 --bs 24    #  96k OOM
