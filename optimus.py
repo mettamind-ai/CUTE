97169e30312e26e70f27c83da6969eb2b578f4b2
Tien Dung <dungtn@gmail.com>
2025-05-29 12:15:11 +0000

    fix

--- optimus.py
+++ optimus.py
@@ -152,7 +152,7 @@ def _dynamic_int8_mm(A: Tensor, B: Tensor, sr=False, hack=False, quant=False) ->
     A_i8, row_scale = quantize_int8(A, dim=1, sr=Asr)
     B_t_i8, col_scale = quantize_int8(B.T, dim=1, sr=Bsr)
 
-    dtype = torch.float32 if quant els A.dtype
+    dtype = torch.float32 if quant else A.dtype
     C = torch.empty(A.shape[0], B.shape[1], device=A.device, dtype=dtype)
 
     scaled_mm(
