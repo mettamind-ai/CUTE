ffb857dd8e52295df8776742954d6eb233222ee6
Tien Dung <dungtn@gmail.com>
2025-05-30 12:41:22 +0000

    try 4bit matmul on grad weight

--- optimus.py
+++ optimus.py
@@ -4,9 +4,12 @@ INT8 Mixed Precision modded from github.com/gau-nernst/quantized-training
 Muon optimizer modded from github.com/nil0x9/flash-muon
 '''
 
-##########################
-##  INT8 Triton Matmul  ##
-##########################
+###########################
+##  INT4 Cutlass Matmul  ##
+###########################
+
+from pathlib import Path
+import torch.utils.cpp_extension
 
 import torch, triton, os
 import triton.language as tl
@@ -14,6 +17,61 @@ from torch import Tensor
 
 lib = torch.library.Library("qtrain", "DEF")
 lib_ops = torch.ops.qtrain
+CURRENT_DIR = Path(__file__).parent
+
+_cutlass_mm = torch.utils.cpp_extension.load(
+    "cutlass_mm",
+    sources=[CURRENT_DIR / "speed/cutlass_mm.cu"],
+    extra_cuda_cflags=["-O3"],
+    extra_include_paths=[str(CURRENT_DIR / "speed/third-party/cutlass/include")],
+    verbose=True,
+)
+
+lib.define("int4_mm(Tensor A, Tensor B) -> Tensor")
+def int4_mm(A: Tensor, B: Tensor) -> Tensor:
+    assert A.is_cuda and A.ndim == 2 and A.dtype is torch.int8 and A.is_contiguous()
+    assert B.is_cuda and B.ndim == 2 and B.dtype is torch.int8 and B.T.is_contiguous()
+    return lib_ops.int4_mm(A, B)
+
+
+@torch.library.impl(lib, "int4_mm", "Meta")
+def _(A: Tensor, B: Tensor) -> Tensor:
+    return torch.empty((A.shape[0], B.shape[1]), device=A.device, dtype=torch.int32)
+
+
+torch.library.impl(lib, "int4_mm", "CUDA")(_cutlass_mm.int4_mm)
+lib.define("scaled_int4_mm(Tensor A, Tensor B, Tensor row_scale, Tensor col_scale) -> Tensor")
+def scaled_int4_mm(A: Tensor, B: Tensor, row_scale: Tensor, col_scale: Tensor) -> Tensor:
+    assert A.is_cuda and A.ndim == 2 and A.dtype is torch.int8
+    assert B.is_cuda and B.ndim == 2 and B.dtype is torch.int8
+    assert row_scale.dtype == col_scale.dtype == torch.bfloat16  # only support bfloat16 for now
+    assert row_scale.squeeze().shape == (A.shape[0],)
+    assert col_scale.squeeze().shape == (B.shape[1],)
+    return lib_ops.scaled_int4_mm(A.contiguous(), B.contiguous(), row_scale.contiguous(), col_scale.contiguous())
+
+
+@torch.library.impl(lib, "scaled_int4_mm", "Meta")
+def _(A: Tensor, B: Tensor, row_scale: Tensor, col_scale: Tensor) -> Tensor:
+    return torch.empty((A.shape[0], B.shape[1]), device=A.device, dtype=row_scale.dtype)
+
+torch.library.impl(lib, "scaled_int4_mm", "CUDA")(_cutlass_mm.scaled_int4_mm)
+
+
+@torch.no_grad()
+def quantize_int4(x: Tensor) -> Tensor:
+    pos_scale = x.relu().amax(dim=1) / 7
+    neg_scale = x.neg().relu().amax(dim=1) / 8
+    scale = torch.maximum(pos_scale, neg_scale)
+    inv_scale = 1.0 / scale.float().clip(1e-12)
+    x = x.float() * inv_scale.view(-1, 1)
+    x = x.round().to(torch.int8)
+    x = (x[:, ::2] << 4) | (x[:, 1::2] & 0xF)
+    return x, scale
+
+
+##########################
+##  INT8 Triton Matmul  ##
+##########################
 
 cfgs, _grid = [ # (BLOCK_M, BLOCK_N, BLOCK_K, num_stages, num_warps)
     (128, 128,  32, 4, 4), (128,  64,  32, 4, 4), ( 64, 128,  32, 4, 4), (128,  32,  32, 4, 4),
@@ -354,9 +412,14 @@ class Int8MixedLinear(torch.autograd.Function):
             ## grad_weight = grad_output.T @ inp; cả 2 là activation nên rất lớn
             ## Áp dụng INT8 matmul ở đây là lợi nhất; sr=False để tránh OOM
             A, B  = grad_output.T, inp
-            A, As = quantize_int8(A, dim=1, sr=False) # không cần round vì grad ko truyền tiếp
-            B, Bs = quantize_int8(B, dim=0, sr=False) # ... nó được update thẳng vào weight
-            grad_weight = scaled_mm(A, B, As, Bs,)
+            # A, As = quantize_int8(A, dim=1, sr=False) # không cần round vì grad ko truyền tiếp
+            # B, Bs = quantize_int8(B, dim=0, sr=False) # ... nó được update thẳng vào weight
+            # grad_weight = scaled_mm(A, B, As, Bs,)
+
+            ## Thử INT4 Matmul
+            A,  row_scale = quantize_int4(A)
+            BT, col_scale = quantize_int4(B.T)
+            grad_weight = scaled_int4_mm(A, BT.T, row_scale, col_scale,)
 
         if ctx.needs_input_grad[2] and ctx.bias: grad_bias = grad_output.sum(0)
         return grad_input, grad_weight, grad_bias
---
+++ speed/install.sh
@@ -0,0 +1,1 @@
+mkdir -p third-party; cd third-party; git clone https://github.com/NVIDIA/cutlass.git
