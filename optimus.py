04e5db0252b9720e3771ccb8cfae511ed2a2be36
Tien Dung <dungtn@gmail.com>
2025-05-30 06:35:34 +0000

    fix

--- optimus.py
+++ optimus.py
@@ -199,11 +199,11 @@ class Int8MixedLinear(torch.autograd.Function):
         ii, ww = ctx.saved_tensors  # input, weigth
         gi = gw = gb = None         # grad_input, grad_weight, grad_bias
 
-        if ctx.needs_input_grad[0]:
-            grad_input = grad_output @ weight
+        if ctx.needs_input_grad[0]: # Grad truyền tiếp, cần độ cx cao 
+            gi = go @ ww
 
         if ctx.needs_input_grad[1]:
-            ''' Đoạn này dễ OOM vì nhân 2 ma trận input và grad_output rất lớn '''
+            ## Đoạn này dễ OOM vì nhân 2 ma trận input và grad_output rất lớn
             it_i8, it_row_scale = quantize_int8(ii.T, dim=1, sr=False)
             go_i8, go_col_scale = quantize_int8(go, dim=0, sr=False)
             gw = scaled_mm(it_i8, go_i8, it_row_scale, go_col_scale,)
