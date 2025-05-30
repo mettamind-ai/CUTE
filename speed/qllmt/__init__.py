2a576038bbb165cea4a083a14baca2630fc15be1
Tien Dung <dungtn@gmail.com>
2025-05-30 03:05:16 +0000

    update

--- speed/qllmt/__init__.py
+++ speed/qllmt/__init__.py
@@ -1,2 +1,23 @@
-from .wrapping_utils import InputFwdHadamardWrapper, wrap_linear_module
+import re
 from .utils import timing_block, pack_int8_to_bf16, unpack_bf16_to_int8
+from .halo_linear import HaloLinear
+
+
+def wrap_linear_module(module, config):
+    kernel = config.get('kernel', 'simulated')
+    if kernel.startswith('halo'):
+        print(f'wrapping with {kernel}')
+        return HaloLinear.from_unquantized(module, hq_config=config)
+    else: # kernel == 'base'
+        print(f'not wrapping since kernel is {kernel}')
+        return module
+
+
+def wrap_model(model, config, exceptions='head'):
+    exceptions = re.compile(rf'{exceptions}')
+    for name, module in model.named_modules():
+        if isinstance(module, torch.nn.Linear) and not exceptions.search(name):
+            wrapped_module = wrap_linear_module(module, config)
+            setattr(wrapped_module, 'layer_name', name)
+            swap_module(model, name, wrapped_module)
+            print(f"{name} wrapped")
