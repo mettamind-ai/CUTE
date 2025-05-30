ef331b004cb5586672edd55a0e443f6fc6cd2556
Tien Dung <dungtn@gmail.com>
2025-05-30 04:16:01 +0000

    update

--- speed/install.sh
+++ speed/install.sh
@@ -12,5 +12,5 @@
 pip install https://github.com/IST-DASLab/gemm-int8/releases/download/v1.0.0/gemm_int8-1.0.0-py3-none-manylinux_2_24_x86_64.whl
 
 ## qllmt from https://github.com/IST-DASLab/HALO
-cd third-party; git clone https://github.com/NVIDIA/cutlass.git
+mkdir -p third-party; cd third-party; git clone https://github.com/NVIDIA/cutlass.git
 cd ../qllmt; ln -s ../third-party .; python3 setup.py install; rm -rf CMakeFiles CMakeCache.txt CPackConfig.cmake CPackSourceConfig.cmake Makefile cmake_install.cmake ctest/ build/ dist/ *.egg-info/ bin/
