3655fc407224f9ae15c2fa3372f690051b87514b
Tien Dung <dungtn@gmail.com>
2025-05-30 05:51:10 +0000

    cleanup

--- speed/qllmt/functional/__init__.py
+++
@@ -1,2 +0,0 @@
-from .quantization import quantize_simulate
-from .hadamard import right_had, left_had
--- speed/qllmt/kernels/include/codebook_quant.h
+++
@@ -1,6 +0,0 @@
-#include <torch/extension.h>
-
-
-torch::Tensor codebook_quantize(torch::Tensor input, torch::Tensor codebook);
-
-torch::Tensor codebook_quantize_f(torch::Tensor input, torch::Tensor codebook);
