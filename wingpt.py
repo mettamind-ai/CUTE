01008ebed07e43474b3bb85795ef2948c2a8e743
Tien Dung <dungtn@gmail.com>
2025-05-31 10:57:11 +0000

    fix

--- wingpt.py
+++ wingpt.py
@@ -295,7 +295,7 @@ class WinGPT(nn.Module):
         print("WinGPT.skip_from", self.skip_from)
 
         self.lm_head = nn.Linear(dim, vocab_size, bias=False)
-        x = torch.zeros_like(self.lm_head.weight, requires_grad=True, torch.dtype=torch.float8_e4m3fn)
+        x = torch.zeros_like(self.lm_head.weight, requires_grad=True, dtype=torch.float8_e4m3fn)
         self.lm_head.weight = nn.Parameter(x)
 
 
