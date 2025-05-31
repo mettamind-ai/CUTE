e8fe0d8f2d34103a1d46b626bd7a964c405eabd7
Tien Dung <dungtn@gmail.com>
2025-05-31 12:56:25 +0000

    update

--- pretrain.py
+++ pretrain.py
@@ -18,10 +18,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--bs", type=int, default=64) # 64k tokens/step works best in most cases
 parser.add_argument("--steps", type=int, default=1000)
 parser.add_argument("--vocab", type=int, default=32000)
+parser.add_argument("--ohmai", type=int, default=2048)
 parser.add_argument("--minloss", type=float, default=0)
 parser.add_argument("--int8ig", type=str, default="head")   # int8 ignore params (`proj|head` => all Linear) 
 parser.add_argument("--schedule", type=json.loads, default={"warmup": 0.05, "decay": 0.15})
-parser.add_argument("--ohmai", type=int, default=2048, choices=[None, 1600, 2048, 2560])
 parser.add_argument("--future", type=int, default=0, choices=range(50))  # % in final loss
 parser.add_argument("--muonlr", type=float, default=0.030)  # default 0.02, modded gpt 0.025
 parser.add_argument("--adamlr", type=float, default=0.003)  # 3e-4
