135190106c28a7a21821d6e034a1376e8a18d231
Tien Dung <dungtn@gmail.com>
2025-06-02 05:36:04 +0000

    Chốt fused loss fun + compiled model is BEST COMBINE

--- pretrain.py
+++ pretrain.py
@@ -180,11 +180,8 @@ adam_lr_schedule = LRSchedule(args.adamlr, args.steps, **args.schedule)
 #############################
 ## LOSS FUNCTION & PREPARE ##
 #############################
-from wingpt import fused_loss_fn
-if args.C:
-    lossf = fused_loss_fn
-    model = torch.compile(model); print(">>> torch.compile(model) <<<")
-    lossf = torch.compile(lossf); print(">>> torch.compile(lossf) <<<")
+from wingpt import fused_loss_fn as lossf
+if args.C: model = torch.compile(model); print(">>> torch.compile(model) <<<")
 
 print0(f"""\nCHUẨN BỊ HUẤN LUYỆN:
 * GPU(s) {world_size}
