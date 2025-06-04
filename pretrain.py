488d432fdbc6fc8fcfe1b6bcdab059e62ad8d272
Tien Dung <dungtn@gmail.com>
2025-06-04 04:52:34 +0000

    refine

--- pretrain.py
+++ pretrain.py
@@ -172,11 +172,9 @@ adam_lr_schedule = LRSchedule(args.adamlr, args.steps, **args.schedule)
 #############################
 from wingpt import fused_loss_fn as lossf
 if args.C:
-    mode = "max-autotune"
+    mode = "reduce-overhead"
     lossf = torch.compile(lossf, mode=mode)
-    for i, x in enumerate(model.blocks):
-        if i % 2 == 0: x.compile()
-        else: x.compile(mode=mode)
+    for x in model.blocks: x.compile(mode=mode)
 
 print0(f"""\nCHUẨN BỊ HUẤN LUYỆN:
 * GPU(s) {world_size}
