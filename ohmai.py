6b7d55551fd2a7603c2f583f3808011c001116f6
Tien Dung <dungtn@gmail.com>
2025-06-06 08:46:56 +0000

    fix

--- ohmai.py
+++ ohmai.py
@@ -104,7 +104,7 @@ MAX_ACTIVE_VOCAB = 1024 * 32  # 32k tối ưu cho speed, và vừa đủ 1:3 -> 
 class OhMaiHead(nn.Module):
     def __init__(self, dim, vocab, bias=None):
         super().__init__()
-        self.active_vocab = triton.next_power_of_2(vocab / 3)
+        self.active_vocab = triton.next_power_of_2(vocab // 3)
 
         if  self.active_vocab > MAX_ACTIVE_VOCAB:
             self.active_vocab = MAX_ACTIVE_VOCAB
