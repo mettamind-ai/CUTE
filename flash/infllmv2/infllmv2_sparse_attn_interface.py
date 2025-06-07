7b93b59b33de5902ec6790befdbc8dc252c35aaf
Tien Dung <dungtn@gmail.com>
2025-06-07 23:55:36 +0000

    update

--- flash/infllmv2/infllmv2_sparse_attn_interface.py
+++ flash/infllmv2/infllmv2_sparse_attn_interface.py
@@ -14,10 +14,12 @@ from pathlib import Path
 
 os.environ['TORCH_CUDA_ARCH_LIST'] = "8.6;8.9"  # 3050ti, 4090
 free_memory_gb = round(psutil.virtual_memory().available / (1024 ** 3))
-max_jobs = round(free_memory_gb / 9)
-if free_memory_gb > 28: max_jobs += 1
-print(f"infllmv2: free_memory_gb {free_memory_gb}, max_jobs {max_jobs}")
-os.environ["MAX_JOBS"] = str(max_jobs)
+if not os.environ.get("MAX_JOBS"):
+    max_jobs = round(free_memory_gb / 9)
+    if free_memory_gb > 28: max_jobs += 2
+    os.environ["MAX_JOBS"] = str(max_jobs)
+print(f"infllmv2: free_memory_gb {free_memory_gb}, max_jobs {os.environ["MAX_JOBS"]}")
+
 
 NVCC_FLAGS = [
     "-O3", "-std=c++17",
