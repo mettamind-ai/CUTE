ad76fe155fcd5af56e0201825e713cb3be9b8aca
Tien Dung <dungtn@gmail.com>
2025-06-07 12:52:22 +0000

    update

--- flash/infllmv2/__init__.py
+++ flash/infllmv2/__init__.py
@@ -1,9 +1,5 @@
 __version__ = "0.1.0"
 
-from .flash_attn_interface import (
-    flash_attn_varlen_func,
-    flash_attn_with_kvcache,
-)
 from .infllmv2_sparse_attn_interface  import (
     blockmask_to_uint64,
     topk_to_uint64,
--- flash/infllmv2/infllmv2_sparse_attn_interface.py
+++ flash/infllmv2/infllmv2_sparse_attn_interface.py
@@ -16,7 +16,7 @@ os.environ['TORCH_CUDA_ARCH_LIST'] = "8.6;8.9"  # 3050ti, 4090
 free_memory_gb = round(psutil.virtual_memory().available / (1024 ** 3))
 max_jobs = round(free_memory_gb / 9)
 if free_memory_gb > 28: max_jobs += 1
-print(f"flash/attn: free_memory_gb {free_memory_gb}, max_jobs {max_jobs}")
+print(f"infllmv2: free_memory_gb {free_memory_gb}, max_jobs {max_jobs}")
 os.environ["MAX_JOBS"] = str(max_jobs)
 
 NVCC_FLAGS = [
