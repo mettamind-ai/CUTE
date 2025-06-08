ec7f6eb68eed68a19de192c06358a01916a017a6
Tien Dung <dungtn@gmail.com>
2025-06-08 08:50:19 +0000

    update

--- flash/attn/__init__.py
+++ flash/attn/__init__.py
@@ -1,11 +1,7 @@
 __version__ = "2.7.3" # bản này cài được với torch==2.7.1 từ pip
 
 from .flash_attn_interface import (
-    # flash_attn_func,
-    # flash_attn_kvpacked_func,
-    # flash_attn_qkvpacked_func,
+    flash_attn_func,
     flash_attn_varlen_func,
-    # flash_attn_varlen_kvpacked_func,
-    # flash_attn_varlen_qkvpacked_func,
-    # flash_attn_with_kvcache,
+    flash_attn_with_kvcache,
 )
