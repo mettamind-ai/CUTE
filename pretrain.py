39122243ebf5ded0c3703fed98e56d7ab3956108
Tien Dung <dungtn@gmail.com>
2025-06-10 02:17:22 +0000

    update

--- pretrain.py
+++ pretrain.py
@@ -16,7 +16,7 @@ parser.add_argument("--bs", type=int, default=64) # 64k tokens/step works best i
 parser.add_argument("--steps", type=int, default=1000)
 parser.add_argument("--vocab", type=int, default=6400)      # nên là luỹ thừa của 2 (1k, 2k, 4k, 8k, 16k, 32k, 64k ..) ...
 parser.add_argument("--ohmai", type=int, default=None)      # ... để dùng hết cache line mỗi lần triton đọc dữ liệu
-parser.add_argument("--int8ig", type=str, default="head")   # int8 ignore params (for wingpt, 'proj|head' => all Linear) 
+parser.add_argument("--int8ig", type=str, default="head|future") # int8 ignore params (for wingpt, 'proj|head' => all Linear) 
 parser.add_argument("--schedule", type=json.loads, default={"warmup": 0.05, "decay": 0.15})
 parser.add_argument("--muonlr", type=float, default=0.030)  # default 0.02, modded gpt 0.025
 parser.add_argument("--adamlr", type=float, default=0.003)  # 3e-4
