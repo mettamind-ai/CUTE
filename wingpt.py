94d4c5e8ed1213ba1b97bb9100ab32c6e89dc1c4
Tien Dung <dungtn@gmail.com>
2025-06-11 02:30:17 +0000

    update

--- wingpt.py
+++ wingpt.py
@@ -236,7 +236,6 @@ class WinGPT(nn.Module):
         outputs = []
         for i, blk in enumerate(self.blocks):
             keep_this_layer = ( self.steps % self.n_layers != i ) # mỗi step bỏ 1 layer
-            blk.requires_grad_(keep_this_layer)
             if i in self.skip_from: x = x + skip_weights[self.skip_from[i]] * outputs[self.skip_from[i]]
             def fwd(blk): return lambda inp: blk(inp, x0, v_embs, te_lambdas, ve_lambdas, cu_seqlens, max_seqlen, self.rotary)
             y = checkpoint(fwd(blk), x, use_reentrant=False)
