43c69549ad7dc1ad9bbcf2f1fe488580d86c9ef5
Tien Dung <dungtn@gmail.com>
2025-06-14 11:15:02 +0000

    update

--- optimus.py
+++ optimus.py
@@ -21,13 +21,18 @@ cfgs = [triton.Config(dict(BLOCK_M=m, BLOCK_N=n, BLOCK_K=k), num_stages=s, num_w
 [(128, 128, 32, 4, 4), ( 64, 128, 32, 4, 8), (128,  64, 32, 4, 8), (256, 128, 64, 4, 8), (128, 256, 64, 4, 8)]]
 @triton.autotune(configs=cfgs, key=["M", "N", "K", "stride_ak", "stride_bk"])
 @triton.jit
-def _scaled_mm_kernel(
-    A_ptr, B_ptr, C_ptr, A_scale_ptr, B_scale_ptr, M, N, K,
-    stride_am: tl.constexpr, stride_ak: tl.constexpr, stride_bk: tl.constexpr, 
-    stride_bn: tl.constexpr, stride_cm: tl.constexpr, stride_cn: tl.constexpr,
-    BLOCK_M:   tl.constexpr, BLOCK_N:   tl.constexpr, BLOCK_K:   tl.constexpr,
-    GROUP_M:   tl.constexpr = 8, # số khối theo chiều M được nhóm lại (để tối ưu L2 cache)
+    A_ptr, B_ptr, C_ptr,
+    row_scale_ptr, col_scale_ptr,
+    M, N, K,
+    stride_am, stride_ak,
+    stride_bk, stride_bn,
+    stride_cm, stride_cn,
+    BLOCK_M: tl.constexpr,
+    BLOCK_N: tl.constexpr,
+    BLOCK_K: tl.constexpr,
+    GROUP_M: tl.constexpr = 8,
 ):
+    # based on triton.ops.matmul
     pid = tl.program_id(0)
     grid_m = (M + BLOCK_M - 1) // BLOCK_M
     grid_n = (N + BLOCK_N - 1) // BLOCK_N
@@ -36,38 +41,40 @@ def _scaled_mm_kernel(
     width = GROUP_M * grid_n
     group_id = pid // width
     group_size = min(grid_m - group_id * GROUP_M, GROUP_M)
-
     pid_m = group_id * GROUP_M + (pid % group_size)
     pid_n = (pid % width) // (group_size)
 
-    # `r` range arrays (rm, rn, rk là các mảng chỉ số)
     rm = pid_m * BLOCK_M + tl.arange(0, BLOCK_M)
     rn = pid_n * BLOCK_N + tl.arange(0, BLOCK_N)
-    rk =                   tl.arange(0, BLOCK_K)
 
-    ram = tl.max_contiguous(tl.multiple_of(rm % M, BLOCK_M), BLOCK_M) # tl.max_contiguous => tối đa BLOCK_M phần tử liền kề trong memory
-    rbn = tl.max_contiguous(tl.multiple_of(rn % N, BLOCK_N), BLOCK_N) # tl.multiple_of => gợi ý alignment, chỉ số là bội số của BLOCK_N
+    ram = tl.max_contiguous(tl.multiple_of(rm % M, BLOCK_M), BLOCK_M)
+    rbn = tl.max_contiguous(tl.multiple_of(rn % N, BLOCK_N), BLOCK_N)
+    rk = tl.arange(0, BLOCK_K)
 
-    A = A_ptr + (ram[:, None] * stride_am +  rk[None, :] * stride_ak) # 2D layout để chuẩn bị nhân ma trận
-    B = B_ptr + ( rk[:, None] * stride_bk + rbn[None, :] * stride_bn)
+    A = A_ptr + (ram[:, None] * stride_am + rk[None, :] * stride_ak)
+    B = B_ptr + (rk[:, None] * stride_bk + rbn[None, :] * stride_bn)
 
-    acc = tl.zeros((BLOCK_M, BLOCK_N), dtype=tl.int32)
-    for _ in range(K, 0, -BLOCK_K):
-        acc += tl.dot(tl.load(A), tl.load(B))
-        A   += BLOCK_K * stride_ak
-        B   += BLOCK_K * stride_bk
+    acc = tl.zeros((BLOCK_M, BLOCK_N), dtype=tl.int32) # ACC_DTYPE = tl.int32
+    for k in range(K, 0, -BLOCK_K):
+        a, b = tl.load(A), tl.load(B)  # EVEN_K = True
+        acc += tl.dot(a, b)
+        A += BLOCK_K * stride_ak
+        B += BLOCK_K * stride_bk
 
-    # Không dùng lại `rm, rn`, mà tính trực tiếp để `rm, rn` được giải phóng ở trước vòng for, tiết kiệm registers
-    idx_m = ( pid_m * BLOCK_M + tl.arange(0, BLOCK_M) )[:, None]
-    idx_n = ( pid_n * BLOCK_N + tl.arange(0, BLOCK_N) )[None, :]
+    # rematerialize rm and rn to save registers
+    rm = pid_m * BLOCK_M + tl.arange(0, BLOCK_M)
+    rn = pid_n * BLOCK_N + tl.arange(0, BLOCK_N)
+    idx_m = rm[:, None]
+    idx_n = rn[None, :]
+    mask = (idx_m < M) & (idx_n < N)
 
-    A_scale = tl.load(A_scale_ptr + idx_m, mask=idx_m < M)
-    B_scale = tl.load(B_scale_ptr + idx_n, mask=idx_n < N)
-    acc = acc.to(tl.float32) * A_scale * B_scale
+    row_scale = tl.load(row_scale_ptr + idx_m, mask=idx_m < M).to(tl.float32)
+    col_scale = tl.load(col_scale_ptr + idx_n, mask=idx_n < N).to(tl.float32)
+    acc = acc.to(tl.float32) * row_scale * col_scale
 
-    mask  = (idx_m < M) & (idx_n < N)
-    index = idx_m * stride_cm + idx_n * stride_cn
-    tl.store(C_ptr + tl.broadcast_to(index, mask.shape), acc, mask)
+    # inductor generates a suffix
+    xindex = idx_m * stride_cm + idx_n * stride_cn
+    tl.store(C_ptr + tl.broadcast_to(xindex, mask.shape), acc, mask)
 
 
 lib.define("scaled_mm(Tensor A, Tensor B, Tensor scale_A, Tensor scale_B) -> Tensor")
@@ -81,6 +88,8 @@ def _(A: Tensor, B: Tensor, scale_A: Tensor, scale_B: Tensor):
 @torch.library.impl(lib, "scaled_mm", "CUDA")
 def _(A: Tensor, B: Tensor, row_scale_A: Tensor, col_scale_B: Tensor):
     M, K = A.shape; _, N = B.shape
+    assert scale_A.is_contiguous()
+    assert scale_B.is_contiguous()
     C = torch.empty(M, N, device=A.device, dtype=row_scale_A.dtype)
     _grid = lambda meta: ( triton.cdiv(meta["M"], meta["BLOCK_M"])*triton.cdiv(meta["N"], meta["BLOCK_N"]), )
     _scaled_mm_kernel[_grid](A, B, C, row_scale_A, col_scale_B, M, N, K, *A.stride(), *B.stride(), *C.stride(),)
