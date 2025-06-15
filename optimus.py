c694e9ca6b464683e62fc0469d8e65883c53c944
Tien Dung <dungtn@gmail.com>
2025-06-15 02:09:31 +0000

    update

--- optimus.py
+++ optimus.py
@@ -11,6 +11,90 @@ import torch.nn.functional as F, torch.utils._pytree as pytree
 from typing import NamedTuple
 from torch import Tensor, nn
 
+###########################
+##  Fused Cross Entropy  ##
+###########################
+
+@triton.jit
+def per_label_cross_entropy(
+        logits_ptr, target_ptr, loss_ptr, reduction,
+        stride:  tl.constexpr, vocab: tl.constexpr,
+        ignore:  tl.constexpr, BLOCK: tl.constexpr,
+    ):
+    pid  = tl.program_id(0).to(tl.int64)  # chạy từ 0 tới num_targets
+    row  = logits_ptr + pid * stride
+    offs = tl.arange(0, BLOCK)
+
+    tgt = tl.load(target_ptr + pid)
+    if tgt == ignore: tl.store(row + offs, 0.0); return
+
+    # softmax(xi) = p(xi) = e^xi / Σ(e^xj) = e^(xi-M) / Σ(e^(xj-M))
+    x    = tl.load(row + offs, mask=offs < vocab, other=-float("inf")).to(tl.float32)
+    M    = tl.max(x, axis=0)
+    e_x  = tl.exp(x - M)        # e^(xi-M)
+    d    = tl.sum(e_x, axis=0)  # Σ(e^(xj-M))
+    lse  = M + tl.log(d)        # log(Σe^logits) => (L)og-(S)um-(E)xp
+
+    grad  = e_x / d             # p(xi) = exp(xi-M) / Σexp(xj-M)
+    grad *= 1 + 2e-5 * lse      # z-loss modification
+    grad  = tl.where(offs == tgt, grad - 1, grad)   # Cross-entropy gradient
+
+    tgt_logit = tl.load(row + tgt).to(tl.float32)   # load trước khi ghi đè grad vào logits
+    tl.store(row + offs, grad * reduction, mask=offs < vocab)
+
+    loss  = lse - tgt_logit     # LCE = Surprise = -log(p_target) = -(x_target - lse)
+    loss += 1e-5*lse*lse        # cộng thêm z_loss penalty giúp ổn định training
+    tl.store(loss_ptr + pid, loss * reduction)                  
+
+
+class FusedCE(torch.autograd.Function):
+    @staticmethod
+    @torch.no_grad()
+    @torch.amp.custom_fwd(device_type="cuda")
+    def forward(ctx, _input, weight, target, n_ignores=0, ignore=-100, ratio=1.0):
+
+        grad_weight = torch.zeros_like(weight, device=_input.device) if weight.requires_grad else None
+        grad_input  = torch.empty_like(_input, device=_input.device)
+        losses      = torch.zeros(_input.shape[0], device=_input.device, dtype=torch.float32)
+
+        n_labels, vocab = _input.shape[0], weight.shape[0]
+        step = min(1024*4, n_labels // 2) # để luôn test được chunked CE
+
+        for s in range( 0, n_labels, step ):
+            e = min(s + step, n_labels)
+            logits = ( _input[s:e] @ weight.t() ).contiguous()
+            per_label_cross_entropy[( logits.shape[0], )](
+                logits_ptr  = logits,
+                target_ptr  = target[s:e],
+                loss_ptr    = losses[s:e],
+                stride      = logits.stride(-2),
+                ignore      = ignore,
+                vocab       = vocab,
+                BLOCK       = triton.next_power_of_2(vocab), 
+                num_warps   = 16 if vocab <= 1024*8 else 32,
+                reduction   = 1.0 / step,
+            )
+            grad_input[s:e] = logits @ weight
+            if weight.requires_grad: grad_weight += logits.t() @ _input[s:e]
+
+        # Khi n_labels lớn thì cộng trước rồi chia sau giúp ổn định số học hơn
+        reduction = ratio * step / (n_labels - n_ignores)
+        ctx.save_for_backward(
+            (grad_input  * reduction).detach(), 
+            (grad_weight * reduction).detach() if weight.requires_grad else None
+        )
+        return torch.sum(losses) * reduction
+
+
+    @staticmethod
+    @torch.amp.custom_bwd(device_type="cuda")
+    def backward(ctx, grad_output, _unused_gi=None, _unused_gw=None):
+        grad_input, grad_weight = ctx.saved_tensors
+        grad_input = grad_input * grad_output
+        if grad_weight is not None: grad_weight = grad_weight * grad_output
+        return grad_input, grad_weight, None, None, None, None
+
+
 ##############################################
 ##  INT8 Mixed Precision for Linear Module  ##
 ##############################################
@@ -123,7 +207,7 @@ class Int8MixedLinear(torch.autograd.Function):
         if ctx.needs_input_grad[1]:
             A, As = quantize_int8(grad_output.T, dim=1, sr=False) # không cần round vì grad ko truyền tiếp
             B, Bs = quantize_int8(inp, dim=0, sr=False)           # ... nó được update thẳng vào weight
-            grad_weight = scaled_mm(A, B, As, Bs, dtype=torch.float16)
+            grad_weight = scaled_mm(A, B, As, Bs, dtype=torch.float32)
 
         return grad_input, grad_weight, grad_bias
 
@@ -170,90 +254,6 @@ def convert_int8_mixed_precision(module:nn.Module, ignore='head'):
     return names, params
 
 
-###########################
-##  Fused Cross Entropy  ##
-###########################
-
-@triton.jit
-def per_label_cross_entropy(
-        logits_ptr, target_ptr, loss_ptr, reduction,
-        stride:  tl.constexpr, vocab: tl.constexpr,
-        ignore:  tl.constexpr, BLOCK: tl.constexpr,
-    ):
-    pid  = tl.program_id(0).to(tl.int64)  # chạy từ 0 tới num_targets
-    row  = logits_ptr + pid * stride
-    offs = tl.arange(0, BLOCK)
-
-    tgt = tl.load(target_ptr + pid)
-    if tgt == ignore: tl.store(row + offs, 0.0); return
-
-    # softmax(xi) = p(xi) = e^xi / Σ(e^xj) = e^(xi-M) / Σ(e^(xj-M))
-    x    = tl.load(row + offs, mask=offs < vocab, other=-float("inf")).to(tl.float32)
-    M    = tl.max(x, axis=0)
-    e_x  = tl.exp(x - M)        # e^(xi-M)
-    d    = tl.sum(e_x, axis=0)  # Σ(e^(xj-M))
-    lse  = M + tl.log(d)        # log(Σe^logits) => (L)og-(S)um-(E)xp
-
-    grad  = e_x / d             # p(xi) = exp(xi-M) / Σexp(xj-M)
-    grad *= 1 + 2e-5 * lse      # z-loss modification
-    grad  = tl.where(offs == tgt, grad - 1, grad)   # Cross-entropy gradient
-
-    tgt_logit = tl.load(row + tgt).to(tl.float32)   # load trước khi ghi đè grad vào logits
-    tl.store(row + offs, grad * reduction, mask=offs < vocab)
-
-    loss  = lse - tgt_logit     # LCE = Surprise = -log(p_target) = -(x_target - lse)
-    loss += 1e-5*lse*lse        # cộng thêm z_loss penalty giúp ổn định training
-    tl.store(loss_ptr + pid, loss * reduction)                  
-
-
-class FusedCE(torch.autograd.Function):
-    @staticmethod
-    @torch.no_grad()
-    @torch.amp.custom_fwd(device_type="cuda")
-    def forward(ctx, _input, weight, target, n_ignores=0, ignore=-100, ratio=1.0):
-
-        grad_weight = torch.zeros_like(weight, device=_input.device) if weight.requires_grad else None
-        grad_input  = torch.empty_like(_input, device=_input.device)
-        losses      = torch.zeros(_input.shape[0], device=_input.device, dtype=torch.float32)
-
-        n_labels, vocab = _input.shape[0], weight.shape[0]
-        step = min(1024*4, n_labels // 2) # để luôn test được chunked CE
-
-        for s in range( 0, n_labels, step ):
-            e = min(s + step, n_labels)
-            logits = ( _input[s:e] @ weight.t() ).contiguous()
-            per_label_cross_entropy[( logits.shape[0], )](
-                logits_ptr  = logits,
-                target_ptr  = target[s:e],
-                loss_ptr    = losses[s:e],
-                stride      = logits.stride(-2),
-                ignore      = ignore,
-                vocab       = vocab,
-                BLOCK       = triton.next_power_of_2(vocab), 
-                num_warps   = 16 if vocab <= 1024*8 else 32,
-                reduction   = 1.0 / step,
-            )
-            grad_input[s:e] = logits @ weight
-            if weight.requires_grad: grad_weight += logits.t() @ _input[s:e]
-
-        # Khi n_labels lớn thì cộng trước rồi chia sau giúp ổn định số học hơn
-        reduction = ratio * step / (n_labels - n_ignores)
-        ctx.save_for_backward(
-            (grad_input  * reduction).detach(), 
-            (grad_weight * reduction).detach() if weight.requires_grad else None
-        )
-        return torch.sum(losses) * reduction
-
-
-    @staticmethod
-    @torch.amp.custom_bwd(device_type="cuda")
-    def backward(ctx, grad_output, _unused_gi=None, _unused_gw=None):
-        grad_input, grad_weight = ctx.saved_tensors
-        grad_input = grad_input * grad_output
-        if grad_weight is not None: grad_weight = grad_weight * grad_output
-        return grad_input, grad_weight, None, None, None, None
-
-
 #################################################################
 ##  MUON optimizer - MomentUm Orthogonalized by Newton-schulz  ##
 #################################################################
@@ -282,13 +282,14 @@ class Muon1GPU(torch.optim.Optimizer):
             for p in group['params']:                   # với mỗi tham số p trong model
                 if p.grad is None: continue             # bỏ qua nếu không có gradient
 
-                g, st = p.grad.bfloat16(), self.state[p]    # lấy gradient và optim state và ...
+                g, st = p.grad, self.state[p]           # lấy gradient và optim state và ...
                 if 'mm' not in st:                      # ... khởi tạo momentum nếu chưa có
                     st['mm'] = torch.zeros_like(g, dtype=torch.bfloat16)
 
                 st['mm'].lerp_(g, 1 - group['mm'])      # momentum = momentum * 0.95 + gradient * 0.05
                 g = g.lerp_(st['mm'], group['mm'])      # gradient = gradient * 0.05 + momentum * 0.95
 
+                g = g.half() # tăng tốc phép nhân ma trận trong trực giao hoá
                 if g.ndim != 2: g = g.view(len(g), -1)  # 2D hoá
                 g = zeropower_via_newtonschulz5(g)      # Trực giao Newton-Schulz g => g(o)rthogonalized
                 if g.shape != p.shape: g=g.view_as(p)   # Reshape back if needed
