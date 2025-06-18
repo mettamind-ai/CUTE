179f0c5efd23e9fd859cfe0b5b477c9df69f6339
Tien Dung <dungtn@gmail.com>
2025-06-18 12:03:41 +0000

    cleanup

--- wingpt.py
+++ wingpt.py
@@ -150,11 +150,8 @@ class Block(nn.Module):
         self.attn = CausalSelfAttention(dim, num_heads, num_kv_heads, max_seq_len, head_dim, self.long, layer_id)
 
     def forward(self, x, x0, ve, te_lambdas, ve_lambdas, cu_seqlens, max_seqlen, rotary):
-        def prepare(x, x0, te_lambdas):
-            x = te_lambdas[self.layer_id][0] * x + \
-                te_lambdas[self.layer_id][1] * x0   # trộn với tok emb gốc x0
-            return x
-        x = checkpoint(prepare, x, x0, te_lambdas, use_reentrant=False)
+        x = te_lambdas[self.layer_id][0] * x + \
+            te_lambdas[self.layer_id][1] * x0   # trộn với tok emb gốc x0
         x = x + self.mlp(norm(x)) if self.mlp is not None else x
         x = x + self.attn(x, ve[self.layer_id], ve_lambdas[self.layer_id], cu_seqlens, max_seqlen, rotary)
         return x
@@ -204,14 +201,12 @@ class WinGPT(nn.Module):
     def forward(self, input_seq, cu_seqlens, max_seqlen):
         ## Token embeddings
         embs = self.embeds(input_seq.long())
-        # def prepare(embs):
         x = x0 = self.mlp0(norm(embs[..., : self.edim ])) # thu edim về dim
+
         ## Value embeddings, bổ trợ cho value trong attention
         v_embs = embs[..., -self.ve*self.kv_dim : ]
         v_embs = list(v_embs.chunk(self.ve, dim=-1))
         v_embs = v_embs + [None]*(self.n_layers - len(v_embs)) + v_embs # U-shape theo kiểu 0,1,2 ... 0,1,2
-        # return x, x0, v_embs
-        # x, x0, v_embs = checkpoint(prepare, embs, use_reentrant=False)
 
         skip_weights = self.scalars[ : self.n_layers]
         te_lambdas   = self.scalars[1*self.n_layers : 3*self.n_layers].view(-1, 2)
