2d28063d098230cc140bc07eabb11e1a44a48920
Tien Dung <dungtn@gmail.com>
2025-06-20 10:58:04 +0000

    update

--- run.sh
+++ run.sh
@@ -1,15 +1,17 @@
-###################################################################### 
-##  CUDA Toolkit 12.8 and latest torch
-######################################################################
-# wget https://developer.download.nvidia.com/compute/cuda/12.8.0/local_installers/cuda_12.8.0_570.86.10_linux.run
-# sudo sh cuda_12.8.0_570.86.10_linux.run
-# pip install --pre torch==2.8.0.dev20250613 --index-url https://download.pytorch.org/whl/nightly/cu128 -U
-# NVTE_FRAMEWORK=pytorch pip install git+https://github.com/NVIDIA/TransformerEngine.git@stable
+######################################################################## 
+## CUDA Toolkit 12.9, CuTE DSL
+## https://docs.nvidia.com/cutlass/media/docs/pythonDSL/quick_start.html
+########################################################################
+# wget https://developer.download.nvidia.com/compute/cuda/12.9.1/local_installers/cuda_12.9.1_575.57.08_linux.run
+# sudo sh cuda_12.9.1_575.57.08_linux.run
+# conda create -n cute python=3.12; conda activate cute
+# pip install nvidia-cutlass-dsl==4.0.0
 
 ######################################################################
 ## Others, data, test run ...
 ######################################################################
-pip install numpy wandb einops torch==2.6.0 -U --user
+# pip install --pre torch==2.8.0.dev20250613 --index-url https://download.pytorch.org/whl/nightly/cu128 -U
+pip install numpy wandb einops ninja torch==2.6.0 -U --user
 git clone https://github.com/NVIDIA/cutlass.git flash/attn/cutlass
 cd flash/attn/cutlass; git checkout c506e16788cb08416a4a57e11a9067beeee29420;  cd ../../.. # flash_attn 2.7.3
 
