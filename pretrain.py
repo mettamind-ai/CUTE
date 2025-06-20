8d391f440c281f37f05a97558592070958c779b9
Tien Dung <dungtn@gmail.com>
2025-06-20 00:36:39 +0000

    fix

--- pretrain.py
+++ pretrain.py
@@ -84,9 +84,9 @@ class LRSchedule:
 lr_schedule = LRSchedule(args.steps, **args.schedule)
 muon_params = [p for n, p in model.named_parameters() if "proj" in n]
 adam_params = [#p for n, p in model.named_parameters() if "proj" not in n]
-    dict(params=[*model.embeds.parameters()                             ], lr=0.1    ), 
-    dict(params=[ model.layer_skips, model.te_lambdas, model.ve_lambdas ], lr=0.015  ),
-    dict(params=[*model.unembeds.parameters()                           ], lr=1/300  ), ]
+    dict(params=[*model.embeds.parameters()          ], lr=0.1    ), 
+    dict(params=[ model.layer_skips, model.te_lambdas], lr=0.015  ),
+    dict(params=[*model.unembeds.parameters()        ], lr=1/300  ), ]
 adam_optim  = torch.optim.AdamW(adam_params, weight_decay=0.0, fused=True)  # eps=1e-10,
 muon_optim  = Muon(muon_params, lr=0.025, momentum=0.95, weight_decay=0.01)
 for opt in [muon_optim, adam_optim]:
