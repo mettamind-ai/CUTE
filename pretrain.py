9add5e99e944c7eba710738365b9879674929f9d
Tien Dung <dungtn@gmail.com>
2025-06-20 07:15:04 +0000

    update

--- pretrain.py
+++ pretrain.py
@@ -31,7 +31,7 @@ def print0(msg): is_master and print(msg)
 if args.bs is None: # cài đặt mặc định bs cho 4090
     if   args.L:   args.bs = 48
     elif args.M:   args.bs = 64
-    else:          args.bs = 92
+    else:          args.bs = 88
 tokens_per_batch = args.bs*1024
 
 if   args.L: # (L)arge  ~ 680m
@@ -42,7 +42,7 @@ elif args.M: # (M)edium ~ 460m
     model = WinGPT(dim=2048, n_layers=10, num_heads=16, num_kv_heads=4, head_dim=64,
         vocab_size=args.vocab, max_seq_len=tokens_per_batch, active_vocab=args.ohmai,)
 
-else:        # (S)mall  ~ 240m vram params
+else:        # (S)mall  ~ 250m vram params
     model = WinGPT(dim=1024, n_layers=20, num_heads=16, num_kv_heads=4, head_dim=64,
         vocab_size=args.vocab, max_seq_len=tokens_per_batch, active_vocab=args.ohmai,)
 
