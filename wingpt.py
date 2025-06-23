f1103a4c32e7ffa9088db62e743f252607ef5928
Tien Dung <dungtn@gmail.com>
2025-06-23 11:09:11 +0000

    update

--- wingpt.py
+++ wingpt.py
@@ -52,7 +52,7 @@ class Rotary(nn.Module):
 
 
 class ReLuSquareMLP(nn.Module):
-    def __init__(self, dim:int, hdim=None, odim=None, expansion_factor=4, zero_out=True):
+    def __init__(self, dim:int, hdim=None, odim=None, expansion_factor=2, zero_out=True):
         super().__init__()
         if not hdim: hdim = int(dim*expansion_factor)
         if not odim: odim = dim
