e1e486f815b3a91a5845234b6aea781bde1755a4
Tien Dung <dungtn@gmail.com>
2025-06-23 09:28:05 +0000

    fix

--- pretrain.py
+++ pretrain.py
@@ -12,10 +12,10 @@ from tqdm import tqdm
 from torch import Tensor, nn
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--bs",     type=int, default=64)   # 64k tokens/batch works best in most cases
+parser.add_argument("--bs",     type=int, default=96)
 parser.add_argument("--steps",  type=int, default=500)
-parser.add_argument("--vocab",  type=int, default=6400) # nên là luỹ thừa của 2 (1k, 2k, 4k, 8k, 16k, 32k, 64k ..) ...
-parser.add_argument("--ohmai",  type=int, default=2048) # ... để dùng hết cache line mỗi lần triton đọc dữ liệu
+parser.add_argument("--vocab",  type=int, default=6400)
+parser.add_argument("--ohmai",  type=int, default=2048)
 args = parser.parse_args()
 
 torch.manual_seed(1981)
