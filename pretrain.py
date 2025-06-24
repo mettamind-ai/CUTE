52d7f43e8a28ab0eb03434ec0bc46bc1018a659a
Tien Dung <dungtn@gmail.com>
2025-06-24 07:35:45 +0700

    refine

--- pretrain.py
+++ pretrain.py
@@ -20,7 +20,7 @@ args = parser.parse_args()
 torch.manual_seed(1981)
 tokens_per_batch = args.bs*1024
 
-model = WinGPT( dim=1024, expansion=4, n_layers=25, num_heads=16, num_kv_heads=8, head_dim=64,
+model = WinGPT( dim=1024, expansion=3, n_layers=24, num_heads=16, num_kv_heads=8, head_dim=64,
                 vocab_size=args.vocab, max_seq_len=tokens_per_batch) # 360m; config ~= qwen3 0.6b
 
 ## Load data, sooner better
