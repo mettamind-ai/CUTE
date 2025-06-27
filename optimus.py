095f20864881998a526c06e5d9aced891aa200d5
Tien Dung <dungtn@gmail.com>
2025-06-27 03:33:28 +0000

    update

--- optimus.py
+++ optimus.py
@@ -99,6 +99,30 @@ def quantize_int8(tensor, dim=1, eps=1e-12, sr=False):
     return ( tensor, scale )                            # int8, float32
 
 
+def _fp32_to_bf16_sr(x_f32: Tensor) -> Tensor:
+    ''' https://github.com/pytorch/ao/blob/main/torchao/optim/quant_utils.py
+    For an FP32 number      [a31, ..., a16, a15, ..., a0] to be converted to BF16
+    - Round towards zero:   [a31, ..., a16,   0, ...,  0]
+    - Round away from zero: [a31, ..., a16+1, 0, ...,  0]
+    (since the value can be negative, we use round towards/away from zero instead of round up/down)
+    For stochastic rounding, we round away from zero with the probability of
+    [a15, ..., a0] / 2^16, where the bit pattern [a15, ..., a0] is interpreted as uint16  
+    we have to use int32 since most arithmetic ops are not implemented for uint32/int16/uint16
+    '''
+    rand_16bit = torch.randint(0, 1 << 16, x_f32.shape, device=x_f32.device, dtype=torch.int32)
+    x_f32_bits = x_f32.view(torch.int32)
+    x_fraction = x_f32_bits & 0xFFFF              # lower 16 bits
+    x_bf16_towards_zero = x_f32_bits & 0xFFFF0000 # upper 16 bits
+    '''
+    x_f32_bits = torch.where(
+        rand_16bit < x_fraction,                  # this is True with the probability of p_fraction
+        x_bf16_towards_zero + 0x10000,            # this might overflow, which will result in UB due to signed integer
+        x_bf16_towards_zero,
+    ) # '''
+    x_f32_bits = (x_f32_bits + rand_16bit) & 0xFFFF0000  # alternative, slightly faster
+    return x_f32_bits.view(torch.float32).bfloat16()
+
+
 class Int8MixedLinear(torch.autograd.Function):
     @staticmethod
     def forward(inp, weight, bias=None):
@@ -122,22 +146,10 @@ class Int8MixedLinear(torch.autograd.Function):
         grad_input = scaled_mm(A, B, As, Bs, dtype=grad_output.dtype)
 
         if ctx.needs_input_grad[1]:
-            T, D = inp.shape
-            step = min(1024*8, T)
-            assert T % step == 0
-
-            if step == D:
-                A, As = quantize_int8(grad_output.T, dim=1, sr=False) # không cần round vì grad ko truyền tiếp
-                B, Bs = quantize_int8(inp, dim=0, sr=False)           # ... nó được update thẳng vào weight
-                grad_weight = scaled_mm(A, B, As, Bs, dtype=weight.dtype)
-            else:
-                grad_weight = torch.zeros_like(weight, dtype=torch.float32)
-                for i in range(0, T, step):
-                    A, As = quantize_int8(grad_output[i:i+step].T, dim=1, sr=False)
-                    for k in range(0, T, step):
-                        B, Bs = quantize_int8(inp[k:k+step], dim=0, sr=False)
-                        grad_weight = grad_weight + scaled_mm(A, B, As, Bs, dtype=torch.float32)
-                grad_weight = grad_weight.to(weight.dtype)
+            A, As = quantize_int8(grad_output.T, dim=1, sr=False) 
+            B, Bs = quantize_int8(inp, dim=0, sr=False)
+            grad_weight = scaled_mm(A, B, As, Bs, dtype=torch.float32)
+            grad_weight = _fp32_to_bf16_sr(grad_weight)
         return grad_input, grad_weight, grad_bias
 
 
