5fed6b8aa502b24fcf4e8ceca11725a712b7550a
Tien Dung <dungtn@gmail.com>
2025-06-28 00:50:00 +0000

    update

--- pretrain.py
+++ pretrain.py
@@ -24,7 +24,7 @@ torch.manual_seed(1981)
 D, E, HD, T = (512, 2, 64, 192) if args.X else (1024, 2, 128, 96)
 if args.bs is None: args.bs = T
 tokens_per_batch = args.bs*1024
-model = WinGPT(dim=D, expansion=E, n_layers=25, head_dim=HD, vocab_size=args.vocab, ctxlen=tokens_per_batch)
+model = WinGPT(dim=D, expansion=E, n_layers=24, head_dim=HD, vocab_size=args.vocab, ctxlen=tokens_per_batch)
 
 ## Load data, sooner better
 def _load_data_shard(file: Path):
