3a5b0251756da2654646b15439995ffa79273f30
Tien Dung <dungtn@gmail.com>
2025-07-26 03:45:52 +0000

    update

--- flash/sparse/sparse_semi_structured_linear.py
+++
@@ -1,5 +0,0 @@
-import sparse.cutlass
-
-
-def sparse_semi_structured_linear(input, weight, meta, bias_opt=None, activation_opt=None):
-    return sparse.cutlass.my_sparse_semi_structured_linear(input, weight, meta, bias_opt, activation_opt)
